4e48945ccd6dc28658ed69061b16e47e64b567de
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-12-22 21:42:02 -0400

    first commit

---
+++ src/main/java/com/alexandre/alexandrialibrary/AlexandrialibraryApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.alexandrialibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandrialibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandrialibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandre/alexandrialibrary/AlexandrialibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.alexandrialibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandrialibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
