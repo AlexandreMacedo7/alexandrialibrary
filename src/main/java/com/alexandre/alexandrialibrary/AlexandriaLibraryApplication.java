1cdaad433c4edc09a8623bf6fe1e251a76ca32e7
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-12-22 22:02:12 -0400

    style: update application class name

--- src/main/java/com/alexandre/alexandrialibrary/AlexandriaLibraryApplication.java
+++ src/main/java/com/alexandre/alexandrialibrary/AlexandriaLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlexandrialibraryApplication {
+public class AlexandriaLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlexandrialibraryApplication.class, args);
+		SpringApplication.run(AlexandriaLibraryApplication.class, args);
 	}
 
 }
--- src/test/java/com/alexandre/alexandrialibrary/AlexandriaLibraryApplicationTests.java
+++ src/test/java/com/alexandre/alexandrialibrary/AlexandriaLibraryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AlexandrialibraryApplicationTests {
+class AlexandriaLibraryApplicationTests {
 
 	@Test
 	void contextLoads() {
