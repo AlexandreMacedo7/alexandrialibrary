537a285998044e5d10c05199b088fd63ffd334b5
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-12-22 22:26:17 -0400

    feat: create Book entity

---
+++ src/main/java/com/alexandre/alexandrialibrary/model/Book.java
@@ -0,0 +1,100 @@
+package com.alexandre.alexandrialibrary.model;
+
+import com.alexandre.alexandrialibrary.model.util.enums.Genre;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String Author;
+    private String ISBN;
+    private Genre genre;
+    private int numberOfPages;
+    private BookPublisher publisher;
+    private LocalDate publicationDate;
+
+    public Book(Long id, String title, String author, String ISBN, Genre genre, int numberOfPages, BookPublisher publisher, LocalDate publicationDate) {
+        this.id = id;
+        this.title = title;
+        Author = author;
+        this.ISBN = ISBN;
+        this.genre = genre;
+        this.numberOfPages = numberOfPages;
+        this.publisher = publisher;
+        this.publicationDate = publicationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public BookPublisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(BookPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+}
