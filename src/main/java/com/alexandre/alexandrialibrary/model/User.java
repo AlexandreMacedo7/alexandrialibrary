640a1a57ff29cd13373ac62dc67edfafc5bedf1c
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-12-22 22:25:48 -0400

    feat: create user entity

---
+++ src/main/java/com/alexandre/alexandrialibrary/model/User.java
@@ -0,0 +1,79 @@
+package com.alexandre.alexandrialibrary.model;
+
+import com.alexandre.alexandrialibrary.model.util.Address;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String phoneNumber;
+    private String email;
+    private Address address;
+
+    public User(Long id, String name, String cpf, String phoneNumber, String email, Address address) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+}
