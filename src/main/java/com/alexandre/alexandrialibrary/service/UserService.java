de04a6d013341ec2ffde0d408a239c43ff569afe
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-12-30 18:48:47 -0400

    feat: injects user validation

--- src/main/java/com/alexandre/alexandrialibrary/service/UserService.java
+++ src/main/java/com/alexandre/alexandrialibrary/service/UserService.java
@@ -3,25 +3,32 @@ package com.alexandre.alexandrialibrary.service;
 import com.alexandre.alexandrialibrary.model.dto.CreateUserDTO;
 import com.alexandre.alexandrialibrary.repository.UserRepository;
 import com.alexandre.alexandrialibrary.util.mapper.UserMapper;
+import com.alexandre.alexandrialibrary.validation.uservalidation.UserValidator;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final List<UserValidator> validator;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, List<UserValidator> validator) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.validator = validator;
     }
 
     @Transactional
     public void create(CreateUserDTO userDTO){
 
+        validator.forEach(v -> v.validate(userDTO));
 
         var user = userMapper.toEntity(userDTO);
+
         userRepository.save(user);
     }
 }
