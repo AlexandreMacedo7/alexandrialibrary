30911ce8a5683f4d3dfd9b031d6656af510ad11d
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-01 18:15:37 -0400

    feat: adds BookLending mapper

---
+++ src/main/java/com/alexandre/alexandrialibrary/util/mapper/BookLendingMapper.java
@@ -0,0 +1,19 @@
+package com.alexandre.alexandrialibrary.util.mapper;
+
+import com.alexandre.alexandrialibrary.model.BookLending;
+import com.alexandre.alexandrialibrary.model.dto.CreateLendingDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookLendingMapper {
+
+    public BookLending toEntity(CreateLendingDTO lendingDTO){
+
+        BookLending bookLending = new BookLending();
+        bookLending.setDateOfWithdrawal(lendingDTO.dateOfWithdrawal());
+        bookLending.setReturnDate(lendingDTO.returnDate());
+        bookLending.setReturned(false);
+
+        return bookLending;
+    }
+}
