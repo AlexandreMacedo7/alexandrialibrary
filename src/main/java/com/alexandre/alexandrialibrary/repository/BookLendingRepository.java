a8d0b04d7b244bec3962b9a528abe7ca4e3dee60
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-08 10:20:33 -0400

    feat: consults books already borrowed

--- src/main/java/com/alexandre/alexandrialibrary/repository/BookLendingRepository.java
+++ src/main/java/com/alexandre/alexandrialibrary/repository/BookLendingRepository.java
@@ -1,9 +1,19 @@
 package com.alexandre.alexandrialibrary.repository;
 
+import com.alexandre.alexandrialibrary.model.Book;
 import com.alexandre.alexandrialibrary.model.BookLending;
+import com.alexandre.alexandrialibrary.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookLendingRepository extends JpaRepository<BookLending, Long> {
+
+    @Query("SELECT COUNT(bl) > 0 FROM BookLending bl " +
+            "WHERE bl.user = :user " +
+            "AND bl.book = :book " +
+            "AND bl.returned = false")
+    boolean existsActiveLoanForSameUserBook(@Param("user") User user, @Param("book") Book book);
 }
