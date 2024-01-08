8286904d3b4be98944d8214323b57e879e277c3c
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-08 10:19:55 -0400

    feat: adds validation for book lending availability

---
+++ src/main/java/com/alexandre/alexandrialibrary/validation/ledingvalidation/ValidatesBookWithLoanInProgress.java
@@ -0,0 +1,29 @@
+package com.alexandre.alexandrialibrary.validation.ledingvalidation;
+
+import com.alexandre.alexandrialibrary.infra.ValidationException;
+import com.alexandre.alexandrialibrary.model.Book;
+import com.alexandre.alexandrialibrary.model.BookLending;
+import com.alexandre.alexandrialibrary.model.User;
+import com.alexandre.alexandrialibrary.repository.BookLendingRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidatesBookWithLoanInProgress implements LendingValidator<BookLending>{
+
+
+    private final BookLendingRepository lendingRepository;
+
+    public ValidatesBookWithLoanInProgress(BookLendingRepository lendingRepository) {
+        this.lendingRepository = lendingRepository;
+    }
+
+    @Override
+    public void validate(BookLending bookLending) {
+        checksWhetherBookHasAlreadyBeenLoaned(bookLending.getUser(), bookLending.getBook());
+    }
+    private void checksWhetherBookHasAlreadyBeenLoaned(User user, Book book){
+        if (lendingRepository.existsActiveLoanForSameUserBook(user, book)){
+            throw new ValidationException("Usuario já emprestou esse livro");
+        }
+    }
+}
